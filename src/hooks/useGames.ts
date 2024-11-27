cc91cb7d58e5c11c2a654963ed512fbf771bfaa0
Seyyed Javad Razavi <Seyyed Javad Razavi@LAPTOP-9JT1IS0D>
2024-11-27 21:40:47 +0330

    Create Http request

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+  }
+  
+  interface FetchGames {
+    count: number;
+    results: Game[];
+  }
+  
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+    const controller = new AbortController();
+
+    useEffect(() => {
+      apiClient
+        .get<FetchGames>("/games", {signal : controller.signal})
+        .then((res) => setGames(res.data.results))
+        .catch((err) => 
+        {
+            if (err instanceof CanceledError) return;
+            setError("EEEEEEEEEEEroooor " + err.message)
+        });
+
+        return () => controller.abort(); 
+    }, []);
+  
+    return {games, error}
+}
+
+export default useGames;
